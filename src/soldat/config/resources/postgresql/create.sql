582e1fb384598ce79af705c635d3679acc6076eb
Ycnik <ybachmann5@gmail.com>
2026-04-07 17:33:53 +0200

    SQL-Skripte angepasst

--- src/soldat/config/resources/postgresql/create.sql
+++ src/soldat/config/resources/postgresql/create.sql
@@ -1,10 +1,5 @@
 SET default_tablespace = soldatspace;
 
-CREATE TYPE geschlecht AS ENUM ('MAENNLICH', 'WEIBLICH', 'DIVERS');
-CREATE TYPE rang AS ENUM ('REKRUT', 'SOLDAT', 'ELITE-SOLDAT', 'CAPTAIN', 'KOMMANDANT');
-CREATE TYPE schweregrad AS ENUM ('LEICHT', 'MITTEL', 'SCHWER', 'KRITISCH');
-CREATE TYPE waffe AS ENUM ('ODM_GEAR', 'Schrotflinte', 'Klingen');
-
 CREATE TABLE IF NOT EXISTS soldat (
     id            INTEGER GENERATED ALWAYS AS IDENTITY(START WITH 1000) PRIMARY KEY,
     version       INTEGER NOT NULL DEFAULT 0,
@@ -13,7 +8,6 @@ CREATE TABLE IF NOT EXISTS soldat (
     geburtsdatum  DATE CHECK (geburtsdatum < current_date),
     geschlecht    geschlecht,
     rang          rang,
-    verletzungen    JSONB,
     username      TEXT NOT NULL,
     erzeugt       TIMESTAMP NOT NULL,
     aktualisiert  TIMESTAMP NOT NULL
@@ -32,12 +26,12 @@ CREATE TABLE IF NOT EXISTS ausruestung (
 CREATE INDEX IF NOT EXISTS ausruestung_soldat_id_idx ON ausruestung(soldat_id);
 CREATE INDEX IF NOT EXISTS ausruestung_serienummer_idx ON ausruestung(serienummer);
 
-CREATE TABLE IF NOT EXISTS verletzungen (
+CREATE TABLE IF NOT EXISTS verletzung (
     id          INTEGER GENERATED ALWAYS AS IDENTITY(START WITH 1000) PRIMARY KEY,
-    verletzungsbezeichnung    TEXT NOT NULL CHECK,
+    verletzungsbezeichnung    TEXT NOT NULL,
     behandelt   boolean NOT NULL,
     schweregrad   schweregrad NOT NULL,
     verletzungsdatum  DATE NOT NULL CHECK (verletzungsdatum <= current_date),
     soldat_id  INTEGER NOT NULL REFERENCES soldat ON DELETE CASCADE
 );
-CREATE INDEX IF NOT EXISTS verletzungen_soldat_id_idx ON verletzungen(soldat_id);
+CREATE INDEX IF NOT EXISTS verletzung_soldat_id_idx ON verletzung(soldat_id);
--- src/soldat/config/resources/postgresql/drop.sql
+++ src/soldat/config/resources/postgresql/drop.sql
@@ -1,13 +1,13 @@
 DROP INDEX IF EXISTS
     ausruestung_soldat_id_idx,
     ausruestung_serienummer_idx,
-    verletzungen_soldat_id_idx,
+    verletzung_soldat_id_idx,
     soldat_nachname_idx;
 
 DROP TABLE IF EXISTS
-    verletzung,
     ausruestung,
-    soldat;
+    verletzung,
+    soldat CASCADE;
 
 DROP TYPE IF EXISTS
     geschlecht,
