a1183b3dd717705e449fa67f215605272830f01d
Ycnik <ybachmann5@gmail.com>
2026-04-09 20:21:14 +0200

    Konfig für graphql

---
+++ src/soldat/config/graphql.py
@@ -0,0 +1,17 @@
+
+"""Konfiguration für GraphQL."""
+
+from typing import Final
+
+from strawberry.http.ides import GraphQL_IDE
+
+from soldat.config.config import app_config
+
+__all__ = ["graphql_ide"]
+
+
+_graphql_toml: Final = app_config.get("graphql", {})
+_graphiql_enabled: Final = bool(_graphql_toml.get("graphiql-enabled", False))
+
+graphql_ide: Final[GraphQL_IDE | None] = "graphiql" if _graphiql_enabled else None
+"""String 'graphiql', falls GraphiQL aktiviert ist, sonst None."""
