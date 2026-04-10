3d2c214a5a0f87af2944f7c89a01088f891e5ccd
Tareq <tareqdg23@gmail.com>
2026-04-10 12:20:59 +0200

    SoldatWriteService im service paket exportiert

--- src/soldat/service/__init__.py
+++ src/soldat/service/__init__.py
@@ -5,10 +5,12 @@ from soldat.service.exceptions import (
 )
 from soldat.service.soldat_dto import SoldatDTO
 from soldat.service.soldat_service import SoldatService
+from soldat.service.soldat_write_service import SoldatWriteService
 
 # https://docs.python.org/3/tutorial/modules.html#importing-from-a-package
 __all__ = [
     "NotFoundError",
     "SoldatDTO",
     "SoldatService",
+    "SoldatWriteService",
 ]
