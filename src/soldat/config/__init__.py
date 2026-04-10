ee714cfd6781a43fb4c193026710bac4869a6e68
Ycnik <ybachmann5@gmail.com>
2026-04-10 14:32:52 +0200

    Keycloak Config

--- src/soldat/config/__init__.py
+++ src/soldat/config/__init__.py
@@ -8,10 +8,16 @@ from soldat.config.db import (
     db_url_admin,
 )
 from soldat.config.dev_modus import dev_db_populate, dev_keycloak_populate
+from soldat.config.keycloak import (
+    csv_config,
+    keycloak_admin_config,
+    keycloak_config,
+)
 from soldat.config.server import host_binding, port
 from soldat.config.tls import tls_certfile, tls_keyfile
 
 __all__ = [
+    "csv_config",
     "db_connect_args",
     "db_dialect",
     "db_log_statements",
@@ -20,6 +26,8 @@ __all__ = [
     "dev_db_populate",
     "dev_keycloak_populate",
     "host_binding",
+    "keycloak_admin_config",
+    "keycloak_config",
     "port",
     "tls_certfile",
     "tls_keyfile",
