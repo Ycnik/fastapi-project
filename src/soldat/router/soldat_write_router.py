1cf28901d116a5ba6c9d94da9fe75855de0d8a05
Tareq <tareqdg23@gmail.com>
2026-04-10 21:30:50 +0200

    feat:post in soldatWriteRouter implementiert

--- src/soldat/router/soldat_write_router.py
+++ src/soldat/router/soldat_write_router.py
@@ -2,10 +2,11 @@
 
 from typing import Annotated, Final
 
-from fastapi import APIRouter, Depends, Response, status
+from fastapi import APIRouter, Depends, Request, Response, status
 from loguru import logger
 
 from soldat.router.dependencies import get_write_service
+from soldat.router.soldat_model import SoldatModel
 from soldat.service import SoldatWriteService
 
 __all__ = ["soldat_write_router"]
@@ -31,3 +32,28 @@ def delete_by_id(
     logger.debug("soldat_id={}", soldat_id)
     service.delete_by_id(soldat_id=soldat_id)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@soldat_write_router.post("")
+def post(
+    soldat_model: SoldatModel,
+    request: Request,
+    service: Annotated[SoldatWriteService, Depends(get_write_service)],
+) -> Response:
+    """POST-Request, um einen neuen Soldaten anzulegen.
+
+    :param soldat_model: Soldatendaten als Pydantic-Model
+    :param request: Injiziertes Request-Objekt von FastAPI bzw. Starlette
+        mit der Request-URL
+    :param service: Injizierter Service für Geschäftslogik
+    :rtype: Response
+    :raises ValidationError: Falls es bei Pydantic Validierungsfehler gibt
+    """
+    logger.debug("soldat_model={}", soldat_model)
+    soldat_dto: Final = service.create(soldat=soldat_model.to_soldat())
+    logger.debug("soldat_dto={}", soldat_dto)
+
+    return Response(
+        status_code=status.HTTP_201_CREATED,
+        headers={"Location": f"{request.url}/{soldat_dto.id}"},
+    )
