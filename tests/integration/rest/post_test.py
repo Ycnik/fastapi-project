32e35d7e17d5af9d272e95119309016c2e0f5338
Ycnik <ybachmann5@gmail.com>
2026-04-14 13:39:12 +0200

    feat: post test

---
+++ tests/integration/rest/post_test.py
@@ -0,0 +1,99 @@
+# ruff: noqa: S101, D103
+"""Tests für POST."""
+
+from http import HTTPStatus
+from re import search
+from typing import Final
+
+from common_test import ctx, rest_url
+from httpx import post
+from pytest import mark
+
+token: str | None
+
+
+@mark.rest
+@mark.post_request
+def test_post() -> None:
+    # arrange
+    neuer_soldat: Final = {
+        "vorname": "VornameRest",
+        "nachname": "Nachnamerest",
+        "geburtsdatum": "2022-02-01",
+        "geschlecht": "W",
+        "rang": "SOLDAT",
+        "ausruestung": {"waffe": "Klinge", "serienummer": "543"},
+        "verletzungen": [{"verletzungsbezeichnung": "Knochenbruch", "behandelt": "false", "schweregrad": "LEICHT", "verletzungsdatum": "2022-02-01"}],  # noqa: E501
+        "username": "testrest",
+    }
+    headers = {"Content-Type": "application/json"}
+
+    # act
+    response: Final = post(
+        rest_url,
+        json=neuer_soldat,
+        headers=headers,
+        verify=ctx,
+    )
+
+    # assert
+    assert response.status_code == HTTPStatus.CREATED
+    location: Final = response.headers.get("Location")
+    assert location is not None
+    int_pattern: Final = "[1-9][0-9]*$"
+    assert search(int_pattern, location) is not None
+    assert not response.text
+
+
+@mark.rest
+@mark.post_request
+def test_post_invalid() -> None:
+    # arrange
+    neuer_soldat_invalid: Final = {
+        "vorname": "Vorname_falsch",
+        "nachname": "Nachnamerest",
+        "geburtsdatum": "2022-02-01",
+        "geschlecht": "W",
+        "rang": "SOLDAT",
+        "ausruestung": {"waffe": "Klinge", "serienummer": "543"},
+        "verletzungen": [{"verletzungsbezeichnung": "Knochenbruch", "behandelt": "false", "schweregrad": "LEICHT", "verletzungsdatum": "2022-02-01"}],  # noqa: E501
+        "username": "testrestinvalid",
+    }
+    headers = {"Content-Type": "application/json"}
+
+    # act
+    response: Final = post(
+        rest_url,
+        json=neuer_soldat_invalid,
+        headers=headers,
+        verify=ctx,
+    )
+
+    # assert
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+    body = response.text
+    assert "vorname" in body
+    assert "nachname" in body
+    assert "geburtsdatum" in body
+    assert "geschlecht" in body
+    assert "waffe" in body
+
+
+@mark.rest
+@mark.post_request
+def test_post_invalid_json() -> None:
+    # arrange
+    json_invalid: Final = '{"nachname" "Nachname"}'
+    headers = {"Content-Type": "application/json"}
+
+    # act
+    response: Final = post(
+        rest_url,
+        json=json_invalid,
+        headers=headers,
+        verify=ctx,
+    )
+
+    # assert
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+    assert "should be a valid dictionary" in response.text
